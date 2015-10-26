f4b7f9d83495477e025f73dee15d58b811ea7aa5
bsspirit <bsspirit@163.com>
2015-10-26 21:18:10 +0800

    'v0.2'

--- R/uibe.r
+++ R/uibe.r
@@ -12,26 +12,24 @@ host<-list(
 
 #' Qutke Data API
 apis<-list(
-  TradingDay=paste(host$host1,'tradingDay',sep='/'),
-  KeyMap=paste(host$host1,'keymap',sep='/'),
-  stock=paste(host$host1,'mktData',sep='/'),
-  stockFwd=paste(host$host1,'mktFwdDaily',sep='/'),
-  index=paste(host$host1,'mktDataIndex',sep='/'),
-  industry=paste(host$host1,'industry',sep='/'),
-  fund=paste(host$host1,'fund',sep='/')
+  tradingDay=paste(host$host1,'tradingDay',sep='/'),
+  keyMap=paste(host$host1,'keyMap',sep='/'),
+  mktDaily=paste(host$host1,'mktDaily',sep='/'),
+  mktFwdDaily=paste(host$host1,'mktFwdDaily',sep='/'),
+  mktDataIndex=paste(host$host1,'mktDataIndex',sep='/'),
+  industryType=paste(host$host1,'industryType',sep='/'),
+  financialIndex=paste(host$host1,'financialIndex',sep='/')
 )
 
 #' Initiate the uibe project 
 #' @title Initiate
 #' @param key character
-#' @return nothing
 #' @author Dan Zhang
 #' 
 #' @examples
 #' init(key='aaa')
 #' 
 #' @export 
-#'
 init <- function (key) {
   options(stringsAsFactors = FALSE)
   if(is.null(key)) stop("ERROR: Key is not empty!")
@@ -40,60 +38,66 @@ init <- function (key) {
 }
 
 
-#' 
 #' Get data from HTTP
 #' @title Get data from HTTP
 #' @importFrom httr GET
 #' @importFrom httr content
 #' 
-#' @param name character
-#' @param args list
+#' @param data character
 #' @param key character
-#' @return data.frame
+#' @param qtid character
+#' @param date character
+#' @param sw1 character
+#' @param sw2 character
+#' @param sw3 character
+#' 
+#' @return list
 #' @author Dan Zhang
 #' 
 #' @examples
 #' \dontrun{
-#' TradingDay<-getData('TradingDay',key='xxxx')
+#' TradingDay<-getData('tradingDay',key='xxx')
 #' 
-#' KeyMap1<-getData('KeyMap',args=list(qtid='000001.SZ'),key='xxxx')
+#' KeyMap1<-getData(data='keyMap',qtid='000001.SZ',key='xxx')
 #'
-#' stock1<-getData('stock',args=list(qtid='000001.SZ'),key='xxxx')
-#' stock2<-getData('stock',args=list(date='2015-09-29'),key='xxxx')
-#' stock3<-getData('stock',args=list(qtid='000001.SZ',date='2015-09-29'),key='xxxx')
+#' stock1<-getData(data='mktDaily',qtid='000001.SZ',key='xxx')
+#' stock2<-getData(data='mktDaily',date='2015-09-29',key='xxx')
+#' stock3<-getData(data='mktDaily',qtid='000001.SZ',date='2015-09-29',key='xxx')
 #'
-#' stockF1<-getData('stockFwd',args=list(qtid='000001.SZ'),key='xxxx')
-#' stockF2<-getData('stockFwd',args=list(date='2015-09-29'),key='xxxx')
-#' stockF3<-getData('stockFwd',args=list(qtid='000001.SZ',date='2015-09-29'),key='xxxx')
+#' stockF1<-getData(data='mktFwdDaily',qtid='000001.SZ',key='xxx')
+#' stockF2<-getData(data='mktFwdDaily',date='2015-09-29',key='xxx')
+#' stockF3<-getData(data='mktFwdDaily',qtid='000001.SZ',date='2015-09-29',key='xxx')
 #'
-#' index1<-getData('index',args=list(qtid='000001.SH'),key='xxxx')
-#' index2<-getData('index',args=list(date='2015-09-29'),key='xxxx')
-#' index3<-getData('index',args=list(qtid='000001.SH',date='2015-09-29'),key='xxxx')
+#' index1<-getData(data='mktDataIndex',qtid='000001.SH',key='xxx')
+#' index2<-getData(data='mktDataIndex',date='2015-09-29',key='xxx')
+#' index3<-getData(data='mktDataIndex',qtid='000001.SH',date='2015-09-29'),key='xxx')
 #'
-#' industry0<-getData('industry',key='xxxx')
-#' industry1<-getData('industry',args=list(qtid='000001.SZ'),key='xxxx')
-#' industry2<-getData('industry',args=list(qtid='000001.SZ',sw1='sw1'),key='xxxx')
-#' industry3<-getData('industry',args=list(qtid='000001.SZ',sw1='sw1',sw2='sw2'),key='xxxx')
-#' industry4<-getData('industry',args=list(qtid='000001.SZ',sw1='sw1',sw2='sw2',sw3='sw3'),key='xxxx')
+#' industry0<-getData(data='industryType',key='xxx')
+#' industry1<-getData(data='industryType',qtid='000001.SZ',key='xxx')
+#' industry2<-getData(data='industryType',qtid='000001.SZ',sw1='sw1',key='xxx')
+#' industry3<-getData(data='industryType',qtid='000001.SZ',sw1='sw1',sw2='sw2',key='xxx')
+#' industry4<-getData(data='industryType',qtid='000001.SZ',sw1='sw1',sw2='sw2',sw3='sw3',key='xxx')
 #'
-#' fund1<-getData('fund',args=list(qtid='000001.SZ'),key='xxxx')
-#' fund2<-getData('fund',args=list(date='2015-09-29'),key='xxxx')
-#' fund3<-getData('fund',args=list(qtid='000001.SZ',date='2015-09-29'),key='xxxx')
+#' fund1<-getData(data='financialIndex',qtid='000001.SZ',key='xxx')
+#' fund2<-getData(data='financialIndex',date='2015-09-29',key='xxx')
+#' fund3<-getData(data='financialIndex',qtid='000001.SZ',date='2015-09-29',key='xxx')
 #' }
 #' 
 #' @export 
-getData<-function(name,args=NULL,key=NULL){
+getData<-function(data,key,qtid=NULL,date=NULL,sw1=NULL,sw2=NULL,sw3=NULL){
   if(is.null(key))  stop("ERROR: Key is not empty!")
 
-  url <- apis[[name]]
-  if(is.null(url))  stop("ERROR: name is not match!")
+  url <- apis[[data]]
+  if(is.null(url))  stop("ERROR: data is not match!")
+  
+  args<-list(qtid=qtid,date=date,sw1=sw1,sw2=sw2,sw3=sw3)
   
   res<-GET(url, query=args)
   return(content(res,encoding='UTF-8'))
-  
 }
 
-#' 
+
+
 #' POST data from HTTP
 #' @title POST data from HTTP
 #' @importFrom httr POST
@@ -105,6 +109,7 @@ getData<-function(name,args=NULL,key=NULL){
 #' @param key character
 #' @return list
 #' @author Dan Zhang
+#' 
 #' @examples
 #' \dontrun{
 #' postData(stock1,name='abc',key='xxxx')
@@ -125,18 +130,15 @@ postData<-function(df,name=NULL,key=NULL){
 }
 
 
-
-#' 
 #' Cast Date to Character
 #' @title Cast Date to Ctring
 #' @param val a date string as yyyy-MM-dd
 #' @return character
-#' @export 
 #' @author Dan Zhang
 #' @examples
 #' as.qtDate()
 #' as.qtDate('2015-10-10')
-#'
+#' @export 
 as.qtDate<-function(val=Sys.Date()){
   as.character(as.numeric(as.POSIXct(as.Date(val)))*1000)
 }
